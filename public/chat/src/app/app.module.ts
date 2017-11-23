be98c527c30ae1e50f0ad51433e08dbd655007f4
Ronald Samy <samy@natexo.com>
2017-11-23 16:46:16 +0100

    create components and services

--- public/chat/src/app/app.module.ts
+++ public/chat/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { ChatUsersComponent } from './chat/chat-users/chat-users.component';
+import { ChatMessagesComponent } from './chat/chat-messages/chat-messages.component';
+import { ChatService } from './chat/chat.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    ChatUsersComponent,
+    ChatMessagesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
